cb4703eefc77a958e28feae20156af8e20d6b22a
npeyrot <npeyrot@ensc.fr>
2025-04-22 17:43:39 +0200

    Update Joueur.cs

--- ENSemenCe/Joueur.cs
+++ ENSemenCe/Joueur.cs
@@ -2,8 +2,8 @@
 
 public class Joueur
 {
-    string Nom {get;set;} // Le nom permet d'offrir une expérience plus immersive dans le jeu.
-    List<Plante> InventaireSemis {get;set;} // Cette liste permet de faire un inventaire des différentes plantes que le joueur peut semer au cours de la partie.
-    List<Terrain> Terrains {get;set;} // Cette liste permet de répertorier les différents terrains possédés par le joueur au cours de la partie.
-    int ActionsRestantes {get;set;} // Il y aura un nombre d'actions possibles par tour, et cette variable permettra d'indiquer à chaque instant le nombre d'actions restantes.
+    public string Nom {get;set;} // Le nom permet d'offrir une expérience plus immersive dans le jeu.
+    public List<Plante> InventaireSemis {get;set;} // Cette liste permet de faire un inventaire des différentes plantes que le joueur peut semer au cours de la partie.
+    public List<Terrain> Terrains {get;set;} // Cette liste permet de répertorier les différents terrains possédés par le joueur au cours de la partie.
+    public int ActionsRestantes {get;set;} // Il y aura un nombre d'actions possibles par tour, et cette variable permettra d'indiquer à chaque instant le nombre d'actions restantes.
 }
