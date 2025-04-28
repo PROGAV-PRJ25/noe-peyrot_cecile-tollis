1554f4d9dcfcfc085340937332361bc8d03269c8
cectollis <ctollis@ensc.fr>
2025-04-28 17:46:33 +0200

    Ajout attribut nom

--- ENSemenCe V2/Joueur.cs
+++ ENSemenCe V2/Joueur.cs
@@ -1,11 +1,13 @@
 public class Joueur
 {
+    public string Nom {get; set;}
     public int NbActionsPossibles {get;set;}
     public List<Plante> InventaireSemis {get;set;}
     public List<string> InventaireRecoltes {get;set;}
 
-    public Joueur()
+    public Joueur(string nom)
     {
+        Nom = nom;
         NbActionsPossibles = 3; // Nombre maximum d'actions par tour : On avait dit 3 ou 4 déjà ? 
         InventaireSemis = new List<Plante>();
         InventaireRecoltes = new List<string>();
