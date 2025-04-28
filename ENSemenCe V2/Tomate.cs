7fd509ca635842f44c3bf47d7e7de28f50a0a349
cectollis <ctollis@ensc.fr>
2025-04-28 18:12:11 +0200

    Update Tomate.cs

--- ENSemenCe V2/Tomate.cs
+++ ENSemenCe V2/Tomate.cs
@@ -3,7 +3,7 @@ public class Tomate : Plante
     public string TerrainPrefere {get;set;}
     public int NbMaxParTerrain = 3; // On avait bien dit 3 ? 
 
-    public Tomate(string nom) : base(nom)
+    public Tomate(string nom, string symbole) : base(nom, symbole)
     {
         TerrainPrefere = "Terre"; // La tomate préfère la terre
     }
