c78423d018dbbd7795dc25156490c66664eae5a7
npeyrot <npeyrot@ensc.fr>
2025-04-28 17:29:03 +0200

    Update TerrainTerre.cs

--- ENSemenCe V2/TerrainTerre.cs
+++ ENSemenCe V2/TerrainTerre.cs
@@ -5,25 +5,4 @@ public class TerrainTerre : Terrain
         TypeTerrain = typeTerrain;
         Cases = new Plante?[nbLignes, nbColonnes];
     }
-
-    public void AfficherTerrainTerre()
-    {
-        for (int i=0; i<Cases.GetLength(0); i++)
-        {
-            for (int j=0; j<Cases.GetLength(1); j++)
-            {
-                if (Cases[i,j] == null)
-                {
-                    Console.Write(" . "); // Case vide.
-                }
-                    
-                else
-                {
-                    Console.Write(" P "); // Case occupée par une plante (P).
-                }       
-            }
-
-            Console.WriteLine();
-        }
-    }
 }
