a68bc0b19cafa6b7251b1a53f92bd922b79bfa59
cectollis <ctollis@ensc.fr>
2025-04-28 17:45:50 +0200

    Test ajout Tomate

--- ENSemenCe V2/Program.cs
+++ ENSemenCe V2/Program.cs
@@ -1,2 +1,11 @@
-﻿Plateau plateau = new Plateau(9,6);
+Plateau plateau = new Plateau(9,6);
+
+// Création d'une Tomate
+Tomate tomate = new Tomate("T");
+
+// Création d'un joueur 
+Joueur joueur = new Joueur("Jean"); // A modifier en demandant le nom au début 
+
+// Ajout de la Tomate dans l'inventaire de semis du joueur
+joueur.InventaireSemis.Add(tomate);
 plateau.AfficherPlateau();
