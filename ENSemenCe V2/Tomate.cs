736dac1d95d3baced68caddb3e3ea0db63db4672
cectollis <ctollis@ensc.fr>
2025-04-30 16:35:23 +0200

    On remplace tomate par rose

--- ENSemenCe V2/Tomate.cs
+++ ENSemenCe V2/Tomate.cs
@@ -1,9 +1,9 @@
-public class Tomate : Plante
+public class Rose : Plante
 {
     public string TerrainPrefere {get;set;}
     public int NbMaxParTerrain = 3; // On avait bien dit 3 ? 
 
-    public Tomate(string nom, string symbole) : base(nom, symbole)
+    public Rose(string nom, string symbole) : base(nom, symbole)
     {
         TerrainPrefere = "Terre"; // La tomate préfère la terre
     }
