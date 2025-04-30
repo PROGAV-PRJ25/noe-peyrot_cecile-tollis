d3c2e145f846d89454a83e6647bb80fbb0701d47
cectollis <ctollis@ensc.fr>
2025-04-30 16:36:39 +0200

    Update Program.cs

--- ENSemenCe V2/Program.cs
+++ ENSemenCe V2/Program.cs
@@ -1,12 +1,17 @@
+/*
 Plateau plateau = new Plateau(9,6);
 
 // Création d'une Tomate
-Tomate tomate = new Tomate("Tomate","T");
+Rose rose = new Rose("Rose", "R");
 
 // Création d'un joueur 
-Joueur joueur = new Joueur("Jean"); // A modifier en demandant le nom au début 
+//Joueur joueur = new Joueur("Jean"); // A modifier en demandant le nom au début 
 
 // Ajout de la Tomate dans l'inventaire de semis du joueur
-joueur.InventaireSemis.Add(tomate);
-joueur.Semer(plateau, "Terre", tomate);
+joueur1.InventaireSemis.Add(rose);
+joueur1.Semer(plateau, "Terre", rose);
 plateau.AfficherPlateau();
+*/
+
+Simulation s = new Simulation(1);
+s.LancerSimulation();
