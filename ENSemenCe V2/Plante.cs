2b25da4cbae47cb9d77ae48940292c7b7b596ab1
cectollis <ctollis@ensc.fr>
2025-05-02 17:58:13 +0200

    Update Plante.cs

--- ENSemenCe V2/Plante.cs
+++ ENSemenCe V2/Plante.cs
@@ -15,11 +15,36 @@ public abstract class Plante
         Nom = nom;
         NiveauCroissance = 0;
         NiveauEau = 100; // La plante n'a pas besoin d'eau au tout début.
-        EtatSante = 100; // la plante est en parfaite santé au tout début.
+        EtatSante = 4; // la plante est en parfaite santé au tout début.
         EstVivante = true;
         EstMure = false;
         EstMalade = false;
     }
+
+    public void AfficherDonnees(Plante plante)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine($"--- {plante.Nom} --- ");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($"    - Niveau d'eau : {plante.NiveauEau}/100"); 
+        if(plante.NiveauEau < 50)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("--> La plante a besoin d'eau ! "); 
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        Console.WriteLine($"    - Etat de santé : {plante.EtatSante}/10"); 
+        if(plante.EtatSante <5)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("         --> La plante n'est pas en bonne santé ! "); 
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        Console.WriteLine();
+        plante.Pousser();
+    }
+
+    public abstract void Pousser();
 }
 
 
