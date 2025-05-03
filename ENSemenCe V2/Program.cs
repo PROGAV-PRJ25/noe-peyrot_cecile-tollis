5d4c586da7d79a3fe7d15e54614f599130dfcf8d
npeyrot <npeyrot@ensc.fr>
2025-05-03 21:31:39 +0200

    Update Program.cs

--- ENSemenCe V2/Program.cs
+++ ENSemenCe V2/Program.cs
@@ -1,17 +1,2 @@
-/*
-Plateau plateau = new Plateau(9,6);
-
-// Création d'une Tomate
-Rose rose = new Rose("Rose", "R");
-
-// Création d'un joueur 
-//Joueur joueur = new Joueur("Jean"); // A modifier en demandant le nom au début 
-
-// Ajout de la Tomate dans l'inventaire de semis du joueur
-joueur1.InventaireSemis.Add(rose);
-joueur1.Semer(plateau, "Terre", rose);
-plateau.AfficherPlateau();
-*/
-
 Simulation s = new Simulation(10);
 s.LancerSimulation();
