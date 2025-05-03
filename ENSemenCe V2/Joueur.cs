38d2b5d68f4a7fcc66bd576662636ce06592bb5c
npeyrot <npeyrot@ensc.fr>
2025-05-03 22:02:53 +0200

    Update Joueur.cs

--- ENSemenCe V2/Joueur.cs
+++ ENSemenCe V2/Joueur.cs
@@ -88,6 +88,76 @@ public class Joueur
 
     public void Recolter(Plateau plateau)
     {
-        // à compléter...
+        Console.WriteLine("Voici les plantes que tu peux récolter :");
+        List<Plante> plantesMures = new List<Plante>();
+
+        int index = 1;
+        foreach (var plante in PlantesSurJardin)
+        {
+            if (plante is Cerise c && c.StadeCroissance==5 || plante is Endive e && e.StadeCroissance==5 || plante is Rose r && r.StadeCroissance==5)
+            {
+                Console.WriteLine($"{index}. {plante.Nom}");
+                plantesMures.Add(plante);
+                index++;
+            }
+        }
+
+        if (plantesMures.Count==0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Aucune plante n'est prête à être récoltée !");
+            Console.ForegroundColor = ConsoleColor.White;
+            return;
+        }
+
+        Console.WriteLine("Choisis la plante à récolter (numéro) :");
+        int choix = Convert.ToInt32(Console.ReadLine()!);
+
+        if (choix<1 || choix>plantesMures.Count)
+        {
+            Console.WriteLine("Numéro invalide !");
+            return;
+        }
+
+        Plante planteARetirer = plantesMures[choix - 1];
+        
+        // Retirer la plante du plateau
+        foreach (var terrain in plateau.Terrains)
+        {
+            for (int i=0; i<terrain.Cases.GetLength(0); i++)
+            {
+                for (int j=0; j<terrain.Cases.GetLength(1); j++)
+                {
+                    if (terrain.Cases[i,j]==planteARetirer)
+                    {
+                        terrain.Cases[i, j] = null;
+                        Console.WriteLine($"{planteARetirer.Nom} a été récoltée !");
+                        PlantesSurJardin.Remove(planteARetirer);
+                        InventaireRecoltes.Add(planteARetirer.Nom);
+                        NbActionsPossibles--;
+                        return;
+                    }
+                }
+            }
+        }
     }
+
+    public void AfficherInventaireRecoltes()
+    {
+        Console.WriteLine("Inventaire des récoltes :");
+
+        if (InventaireRecoltes.Count==0)
+        {
+            Console.WriteLine("Tu n'as encore rien récolté !");
+        }
+
+        else
+        {
+            foreach (var nom in InventaireRecoltes)
+            {
+                Console.WriteLine($"- {nom}");
+            }
+        }
+    }
+
 }
