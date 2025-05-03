be709e72eed7dab9da135284dbd754b31c2d9df4
npeyrot <npeyrot@ensc.fr>
2025-05-03 20:26:08 +0200

    Update Webcam.cs

--- ENSemenCe V2/Webcam.cs
+++ ENSemenCe V2/Webcam.cs
@@ -3,19 +3,23 @@ public class Webcam
     public void AfficherPlateau(Plateau plateau)
     {
         bool afficher = false;
-        Console.WriteLine("Voulez-vous un aperçu de votre jardin ? (Tappez le chiffre correspondant)");
-        Console.WriteLine("1.Oui");
-        Console.WriteLine("2.Non");
+        Console.WriteLine("Voulez-vous un aperçu de votre jardin ? (Tapez le chiffre correspondant)");
+        Console.WriteLine("1. Oui");
+        Console.WriteLine("2. Non");
         int reponse = Convert.ToInt32(Console.ReadLine()!);
+
         if(reponse == 1)
         {
             afficher = true;
         }
+        
         else
         {
             afficher = false;
         }
+
         Console.Clear();
+
         if(afficher)
         {   
             
