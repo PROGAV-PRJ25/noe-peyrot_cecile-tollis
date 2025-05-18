37fbeecb571e76c54b4496a551fac1a44fe6dd86
cectollis <ctollis@ensc.fr>
2025-05-18 02:16:25 +0200

    Update Intrus.cs

--- ENSemenCe/Intrus.cs
+++ ENSemenCe/Intrus.cs
@@ -1,12 +1,14 @@
 public abstract class Intrus
 {
-    public string Nom {get;set;}
-    public string Symbole {get;set;}
+    public string Nom { get; set; }
+    public string Symbole { get; set; }
 
     public Intrus(string nom, string symbole)
     {
         Nom = nom;
         Symbole = symbole;
     }
+    
+    public abstract void Agir(Plateau plateau, Joueur joueur);
 
 }
