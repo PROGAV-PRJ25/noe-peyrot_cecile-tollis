3d11ebe3a19343a6680039ee3249a1cd9430b404
cectollis <ctollis@ensc.fr>
2025-05-18 02:16:49 +0200

    Update Indominus.cs

--- ENSemenCe/Indominus.cs
+++ ENSemenCe/Indominus.cs
@@ -1,9 +1,12 @@
 public class Indominus : Intrus
 {
-    
+
     public Indominus(string nom, string symbole) : base(nom, symbole)
     {
 
     }
+
+    public override void Agir(Plateau plateau, Joueur joueur)
+    {}
 
 }
