6d7672b993e92b422bdf2ec296d53ea076e1e83e
npeyrot <npeyrot@ensc.fr>
2025-05-22 23:04:50 +0200

    Add files via upload

---
+++ ENSemenCe/Sapin.cs
@@ -0,0 +1,105 @@
+public class Sapin : Plante
+{
+    public int NbMaxParTerrain = 2;
+
+    public Sapin(string nom, string symbole) : base(nom, symbole)
+    {
+        TerrainPrefere = "terre";
+    }
+
+    public override void Pousser(Plante plante)
+    {
+        if (plante.EstVivante)
+        {
+            ProgressionCroissance += VitesseDeCroissance;
+
+            while (ProgressionCroissance >= 1 && StadeCroissance < 8)
+            {
+                StadeCroissance++;
+                ProgressionCroissance -= 1;
+            }
+
+            if (StadeCroissance >= 8)
+            {
+                EstMure = true;
+            }
+        }
+    }
+
+    public override void AfficherPlante()
+    {
+        if (StadeCroissance >= 8)
+        {
+            Console.WriteLine("Le sapin est déjà au stade final !");
+            EstMure = true;
+        }
+        else
+        {
+            Console.WriteLine($"Le sapin est au stade {StadeCroissance} de croissance.");
+            Console.WriteLine("--- Aperçu ---");
+            Console.WriteLine();
+        }
+
+        switch (StadeCroissance)
+        {
+            case 1:
+                Console.WriteLine("     ^");
+                break;
+            case 2:
+                Console.WriteLine("     ^");
+                Console.WriteLine("    /|\\");
+                break;
+            case 3:
+                Console.WriteLine("     ^");
+                Console.WriteLine("    /|\\");
+                Console.WriteLine("   //|\\*");
+                break;
+            case 4:
+                Console.WriteLine("     ^");
+                Console.WriteLine("    /|\\");
+                Console.WriteLine("   //|\\*");
+                Console.WriteLine("   /*|\\\\");
+                break;
+            case 5:
+                Console.WriteLine("     ^");
+                Console.WriteLine("    /|\\");
+                Console.WriteLine("   //|\\*");
+                Console.WriteLine("   /*|\\\\");
+                Console.WriteLine("  *//|\\*\\");
+                break;
+            case 6:
+                Console.WriteLine("     ^");
+                Console.WriteLine("    /|\\");
+                Console.WriteLine("   //|\\*");
+                Console.WriteLine("   /*|\\\\");
+                Console.WriteLine("  *//|\\*\\");
+                Console.WriteLine("  /*/*\\\\\\");
+                break;
+            case 7:
+                Console.WriteLine("     ^");
+                Console.WriteLine("    /|\\");
+                Console.WriteLine("   //|\\*");
+                Console.WriteLine("   /*|\\\\");
+                Console.WriteLine("  *//|\\*\\");
+                Console.WriteLine("  /*/*\\\\\\");
+                Console.WriteLine(" /*//|\\*\\\\");
+                break;
+            case 8:
+                Console.WriteLine("     ^");
+                Console.WriteLine("    /|\\");
+                Console.WriteLine("   //|\\*");
+                Console.WriteLine("   /*|\\\\");
+                Console.WriteLine("  *//|\\*\\");
+                Console.WriteLine("  /*/*\\\\\\");
+                Console.WriteLine(" /*//|\\*\\\\");
+                Console.WriteLine(" ///*/|\\*\\*\\\\");
+                Console.WriteLine("/*///*|\\\\*\\\\*\\");
+                Console.WriteLine("       | |");
+                Console.WriteLine("       | |");
+                Console.WriteLine("     /___\\");
+                break;
+            default:
+                break;
+        }
+    }
+}
