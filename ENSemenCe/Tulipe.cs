79181957c28e304bc88abe86ad29f72c320609dd
cectollis <ctollis@ensc.fr>
2025-05-23 18:18:51 +0200

    Update Tulipe.cs

--- ENSemenCe/Tulipe.cs
+++ ENSemenCe/Tulipe.cs
@@ -43,60 +43,157 @@ public class Tulipe : Plante
         switch (StadeCroissance)
         {
             case 1:
-                Console.WriteLine("   /\\");
+                Console.WriteLine(@",");
                 break;
             case 2:
-                Console.WriteLine("   /\\^/`\\");
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
                 break;
             case 3:
-                Console.WriteLine("   /\\^/`\\");
-                Console.WriteLine("    | \\/   |");
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
                 break;
             case 4:
-                Console.WriteLine("   /\\^/`\\");
-                Console.WriteLine("    | \\/   |");
-                Console.WriteLine("    | |    |");
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
                 break;
             case 5:
-                Console.WriteLine("   /\\^/`\\");
-                Console.WriteLine("    | \\/   |");
-                Console.WriteLine("    | |    |");
-                Console.WriteLine("    \\ \\    /");
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
                 break;
             case 6:
-                Console.WriteLine("   /\\^/`\\");
-                Console.WriteLine("    | \\/   |");
-                Console.WriteLine("    | |    |");
-                Console.WriteLine("    \\ \\    /");
-                Console.WriteLine("     '\\\\//'");
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
                 break;
             case 7:
-                Console.WriteLine("   /\\^/`\\");
-                Console.WriteLine("    | \\/   |");
-                Console.WriteLine("    | |    |");
-                Console.WriteLine("    \\ \\    /");
-                Console.WriteLine("     '\\\\//'");
-                Console.WriteLine("       ||");
-                Console.WriteLine("       ||");
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
                 break;
-            case 8: // Stade final
-                Console.WriteLine("   /\\^/`\\");
-                Console.WriteLine("    | \\/   |");
-                Console.WriteLine("    | |    |");
-                Console.WriteLine("    \\ \\    /");
-                Console.WriteLine("     '\\\\//'");
-                Console.WriteLine("       ||");
-                Console.WriteLine("       ||");
-                Console.WriteLine("       ||");
-                Console.WriteLine("       ||  ,");
-                Console.WriteLine("   |\\  ||  |\\");
-                Console.WriteLine("   | | ||  | |");
-                Console.WriteLine("   | | || / /");
-                Console.WriteLine("    \\ \\||/ /");
-                Console.WriteLine("     `\\\\//`");
+            case 8:
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
                 break;
-            default:
+            case 9:
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                break;
+            case 10:
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |  ,");
+                break;
+            case 11:
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |  ,");
+                Console.WriteLine(@"   |\    |  |   |\");
+                break;
+            case 12:
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |  ,");
+                Console.WriteLine(@"   |\    |  |   |\");
+                Console.WriteLine(@"   |  |  |  |   |  |");
+                break;
+            case 13:
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |  ,");
+                Console.WriteLine(@"   |\    |  |   |\");
+                Console.WriteLine(@"   |  |  |  |   |  |");
+                Console.WriteLine(@"   | |   |  |  / /");
+                break;
+            case 14:
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/      |");
+                Console.WriteLine(@"   |     |        |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |  ,");
+                Console.WriteLine(@"   |\    |  |   |\");
+                Console.WriteLine(@"   |  |  |  |   |  |");
+                Console.WriteLine(@"   | |   |  |  / /");
+                Console.WriteLine(@"    \ \ |  |/ /");
+                break;
+            case 15:
+                Console.WriteLine(@",");
+                Console.WriteLine(@"    /  \^/   `\");
+                Console.WriteLine(@"   |     \/     |");
+                Console.WriteLine(@"   |     |       |");
+                Console.WriteLine(@"   \     \      /");
+                Console.WriteLine(@"     '\\    //'");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |");
+                Console.WriteLine(@"         |  |  ,");
+                Console.WriteLine(@"    |\   |  |   |\");
+                Console.WriteLine(@"    |  | |  |   | |");
+                Console.WriteLine(@"    | |  |  |  / /");
+                Console.WriteLine(@"     \ \ |  |/ /");
+                Console.WriteLine(@"      `\ \//`");
+                Console.WriteLine(@"     ^^^^^^^^");
                 break;
         }
+
     }
 }
